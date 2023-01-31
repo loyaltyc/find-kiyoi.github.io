0463314ccf3600dc9dfa981bf18742559cb47f84
loyaltyc <353748255@qq.com>
2023-01-31 16:26:31 +0800

    Add files via upload

--- src/game.js
+++ src/game.js
@@ -50,7 +50,7 @@
             this.stage.enableDOMEvent(Hilo.event.POINTER_START, true);
             this.stage.on(Hilo.event.POINTER_START, this.onUserInput.bind(this));
             //启动计时器
-            this.ticker = new Hilo.Ticker(60);
+            this.ticker = new Hilo.Ticker(180);
             this.ticker.addTick(Hilo.Tween);
             this.ticker.addTick(this.stage);
             this.ticker.start(true);
@@ -63,9 +63,6 @@
 
         initBackground: function () {
             //背景
-            var bgWidth = this.width * this.scale;
-            var bgHeight = this.height * this.scale;
-
             var bgImg = this.asset.bg;
             this.bg = new Hilo.Bitmap({
                 id: 'bg',
@@ -110,6 +107,7 @@
             this.handleClickRock(e);
         },
         handleClickRock(e) {
+            var _this = this;
             var rockId = e.eventTarget.id;
             var checkResult = this.rock.checkBox(rockId);
             // 不可用的格子
@@ -118,10 +116,18 @@
             }
             // 处理格子事件，移动，扣除体力
             var rockData = this.rock.getRockData(checkResult);
+            this.updatePackage(rockData.hiraPackage);
             var pickRock = this.rock.getChildById(rockId);
-            // 平良移动中不可点击，定时器更新状态
+            // 平良移动中不可点击
             this.state = 'moving';
-            var isGameOver = this.hira.startMove(pickRock, rockData.usedHeart);
+            this.hira.startMove(pickRock, rockData);
+        },
+        endMove(isGameOver, rockData) {
+            // 体力耗尽，游戏结束
+            if (isGameOver) {
+                this.gameOver(false)
+                return;
+            }
             // 找到清居，显示奖励提示
             if (rockData.usedHeart === 0) {
                 this.gameOver(true);
@@ -129,24 +135,80 @@
             }
             if (rockData.boxContent !== 'empty') {
                 this.gameRockScene.show(rockData);
-                this.updateStatus('showTip', 800);
             } else {
-                this.updateStatus('playing', 1200);
-            }
-            // 体力耗尽，游戏结束
-            if (isGameOver) {
-                this.gameOver()
+                this.state = 'playing';
             }
         },
-        // 更新状态，防止处理无效点击
-        updateStatus(newStatus, time) {
-            var _this = this;
-            setTimeout(function () {
-                _this.state = newStatus;
-            }, time)
+        // 更新平良背包
+        updatePackage(hiraPackage) {
+            let packageText = '道具：';
+            let itemNameArr = {
+                'camera': '照相机',
+                'gun': '枪',
+                'firework': '烟花',
+                'bicycle': '自行车',
+                'soda': '姜汁汽水',
+                'hat': '帽子'
+            };
+            for (var i = 0; i < hiraPackage.length; i++) {
+                var itemName = itemNameArr[hiraPackage[i]];
+                if (!itemName) {
+                    continue;
+                }
+                packageText += itemName;
+                if (i !== hiraPackage.length - 1) {
+                    packageText += '，'
+                }
+            }
+            new Hilo.DOMElement({
+                id: 'packageText',
+                element: Hilo.createElement('div', {
+                    innerHTML: packageText,
+                    style: {
+                        position: 'absolute',
+                        color: 'rgb(249,195,22)',
+                        fontWeight: 'bolder',
+                        fontSize: '17px',
+                        webkitTextStroke: '1px rgb(63,86,160)'
+                    }
+                }),
+                width: this.width / 3,
+                x: this.width / 7,
+                y: (this.height / 2) - 45
+            }).addTo(this.stage)
         },
+        // 游戏结束
         gameOver(isWin = false) {
-            // 游戏结束，可重新开始
+            let image = isWin ? this.asset.gamewin : this.asset.gameLose;
+            let gameOverText = isWin ? '恭喜！找到老婆！' : '没体力了！<br\>送个熊猫给你吧！';
+            let scaleX = this.width / (image.width * 1.2);
+            let newWidth = image.width * scaleX * 0.5;
+            let tipX = (this.width - newWidth) / 8
+            new Hilo.Bitmap({
+                id: 'gameOverTip',
+                image: image,
+                scaleX: scaleX,
+                scaleY: scaleX,
+                x: tipX,
+                y: 300
+            }).addTo(this.stage);
+            new Hilo.DOMElement({
+                id: 'overText',
+                element: Hilo.createElement('div', {
+                    innerHTML: gameOverText,
+                    style: {
+                        position: 'absolute',
+                        color: 'rgb(249,195,22)',
+                        fontWeight: 'bolder',
+                        fontSize: '40px',
+                        webkitTextStroke: '1px rgb(63,86,160)'
+                    }
+                }),
+                width: this.width - 30,
+                height: 100,
+                x: 55,
+                y:  isWin ? 320 : 280
+            }).addTo(this.stage)
             this.state = 'over'
         },
         initRockScene() {
@@ -155,6 +217,8 @@
                 width: this.width,
                 height: this.height,
                 image: this.asset.tipBg,
+                winImg: this.asset.gamewin,
+                tipImgArr: this.asset.tipImgArr,
                 visible: false
             }).addTo(this.stage);
         },
@@ -164,26 +228,24 @@
             this.initRock();
             this.initHira();
             this.initRockScene();
+            this.updatePackage(['camera'])
             this.state = 'playing'
         },
         initScenes: function () {
             //准备场景
+            let readyTip = this.asset.readyTip;
+            let scaleX = this.width / (readyTip.width * 1.2);
+            let newWidth = readyTip.width * scaleX * 0.5;
+            let tipX = (this.width - newWidth) / 8
             this.gameReadyScene = new game.ReadyScene({
                 id: 'readyScene',
                 width: this.width,
                 height: this.height,
-                image: this.asset.readyTip
+                image: readyTip,
+                x: tipX,
+                y: 100,
+                scaleX: this.width / (readyTip.width * 1.2),
             }).addTo(this.stage);
-
-            // //结束场景
-            // this.gameOverScene = new game.OverScene({
-            //     id: 'overScene',
-            //     width: this.width,
-            //     height: this.height,
-            //     image: this.asset.over,
-            //     numberGlyphs: this.asset.numberGlyphs,
-            //     visible: false
-            // }).addTo(this.stage);
         },
     };
 
